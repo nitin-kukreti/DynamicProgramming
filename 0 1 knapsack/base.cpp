5850416701ade6d8c68991fc4d195fb21b1164e4
Nitin <nitinkukreti12@gmail.com>
2022-04-27 23:12:12 +0530

    first commit

---
+++ 0 1 knapsack/base.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+//  Created by Nitin Kukreti on 2022-04-27.
+// problem is 0/1 knapsack problem
+// we can pick any item only once
+*/
+
+
+struct Item{
+    int weight;
+    int value;
+    Item(int w, int v): weight(w), value(v){}
+};
+
+
+// recursive solution time complexity O(2^n) space complexity O(n)
+int recursive_kapsack(int item,int capacity,vector<Item> &items){
+
+// if we have 0 items or our capacity is 0 then we can't take any items
+    if(item==0 || capacity==0) return 0;
+
+//  when we not take the current item
+  int not_taken=recursive_kapsack(item-1,capacity,items);
+
+ // if the weight of the current item is less than the capacity then we can take it
+  int taken=0;
+  if(items[item-1].weight<=capacity){
+    //  pick and goes to the next item
+    taken=items[item-1].value+recursive_kapsack(item-1,capacity-items[item-1].weight,items);
+  }   
+
+  return max(taken,not_taken);
+}
+
+// recursive memoization time complexity O(n*capacity) space complexity O(n*capacity) + O(n)
+int recursive_kapsackMemo(int item,int capacity,vector<Item> &items,vector<vector<int>> &memo){
+
+// if we have 0 items or our capacity is 0 then we can't take any items
+    if(item==0 || capacity==0) return 0;
+   if(memo[item][capacity]!=-1) return memo[item][capacity];
+
+// when we not take the current item
+  int not_taken=recursive_kapsackMemo(item-1,capacity,items,memo);
+
+ // if the weight of the current item is less than the capacity then we can take it
+  int taken=0;
+  if(items[item-1].weight<=capacity){
+       //  pick and goes to the next item
+    taken=items[item-1].value+recursive_kapsackMemo(item-1,capacity-items[item-1].weight,items,memo);
+  }   
+
+  return memo[item][capacity]=max(taken,not_taken);
+}
+
+// time complexity O(n*capacity) space complexity O(n*capacity)
+int tabular_kapsack(int capacity,vector<Item> &items){
+    int n=items.size();
+    vector<vector<int>> T(n+1,vector<int>(capacity+1,0));
+    
+    // we have profit of 0 if we have 0 items or our capacity is 0
+    for(int i=0;i<=n;i++) T[i][0]=0;
+    for(int i=0;i<=capacity;i++) T[0][i]=0;
+
+    for(int item=1;item<=n;item++){
+        for(int cap=1;cap<=capacity;cap++){
+       
+            //    when we not take the item
+            int not_taken=T[item-1][cap];
+
+            //    when we take the item if the weight of the item is less than the capacity
+            int taken=0;
+            if(items[item-1].weight<=cap){
+                 //  pick and goes to the next item
+                taken=items[item-1].value+T[item-1][cap-items[item-1].weight];
+            }
+
+            // we take the item if the profit is greater than the profit of not taking the item
+            T[item][cap]=max(taken,not_taken);
+
+        }
+    }
+    return T[n][capacity];
+}
+
+
+int maxProfit(int bagSize,vector<Item> &item){
+
+   return tabular_kapsack(bagSize,item);
+}
+
+
+
+
+
+int main()
+{
+    vector<Item> items={Item(2,3),Item(1,2),Item(3,4),Item(2,2),Item(3,5),Item(2,1)};
+    int capacity=5;
+    cout<<maxProfit(capacity,items);
+   
+  
+    return 0;
+}
---
+++ LCS/base.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// recursive solution time complexity O(2^n) space complexity O(max(n,m)) 
+int recursive_lcs(int ithcharS1,int ithcharS2,string s1,string s2){
+    // if any string is empty then common subsequence is empty
+    if(ithcharS1==0 || ithcharS2==0) return 0;
+
+    // not taking the current character
+    int not_taken_s1=recursive_lcs(ithcharS1-1,ithcharS2,s1,s2);
+    int not_taken_s2=recursive_lcs(ithcharS1,ithcharS2-1,s1,s2);
+    int not_taken=max(not_taken_s1,not_taken_s2);
+
+    // taking the current character
+    int taken=0;
+    if(s1[ithcharS1-1]==s2[ithcharS2-1]){
+        taken=1+recursive_lcs(ithcharS1-1,ithcharS2-1,s1,s2);
+    }
+    return max(taken,not_taken);
+} 
+
+// recursive memoization time complexity O(n*m) space complexity O(n*m) + O(max(n,m))
+int memo_lcs(int ithcharS1,int ithcharS2,string s1,string s2,vector<vector<int>> &memo){
+    // if any string is empty then common subsequence is empty
+    if(ithcharS1==0 || ithcharS2==0) return 0;
+    if(memo[ithcharS1][ithcharS2]!=-1) return memo[ithcharS1][ithcharS2];
+
+    // not taking the current character
+    int not_taken_s1=memo_lcs(ithcharS1-1,ithcharS2,s1,s2,memo);
+    int not_taken_s2=memo_lcs(ithcharS1,ithcharS2-1,s1,s2,memo);
+    int not_taken=max(not_taken_s1,not_taken_s2);
+
+    // taking the current character
+    int taken=0;
+    if(s1[ithcharS1-1]==s2[ithcharS2-1]){
+        taken=1+memo_lcs(ithcharS1-1,ithcharS2-1,s1,s2,memo);
+    }
+    return memo[ithcharS1][ithcharS2]=max(taken,not_taken);
+}
+
+int tab_lcs(string s1,string s2){
+    int n=s1.size();
+    int m=s2.size();
+    vector<vector<int>> T(n+1,vector<int>(m+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            int taken=0;
+            if(s1[i-1]==s2[j-1]){
+                taken=1+T[i-1][j-1];
+            }
+            int nottaken=max(T[i-1][j],T[i][j-1]);
+
+            T[i][j]=max(taken,nottaken);
+        }
+    }
+    return T[n][m];
+}
+
+int main()
+{
+    string s1="AGGTAB";
+    string s2="GXTXAYB";
+    int n=s1.size();
+    int m=s2.size();
+    vector<vector<int>> memo(n+1,vector<int>(m+1,-1));
+    cout<<tab_lcs(s1,s2)<<endl;
+    return 0;
+}
